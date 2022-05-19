08a5c1b6588b4ca92aaf3bd8464b74c499ef6524
romainpascual <romain.pascual@laposte.net>
2022-05-19 19:57:04 +0200

    update loader ; performance tests

--- loader.py
+++ loader.py
@@ -4,8 +4,15 @@ import requests
 import bpy
 import json
 import math
+import time
 
-    
+def ShowMessageBox(message = "", title = "Message Box", icon = 'INFO'):
+
+    def draw(self, context):
+        self.layout.label(text=message)
+
+    bpy.context.window_manager.popup_menu(draw, title = title, icon = icon)
+        
 ##  ==================================================================================
 
 def hakim():
@@ -31,12 +38,17 @@ def cylinder_between(x, y, r):
     
     d = [(yi-xi) for xi,yi in zip(x,y)]
     dist = math.sqrt(sum(di**2 for di in d))
+    
+    if dist == 0:
+        return
+    
     mid = tuple((xi+yi)/2 for xi,yi in zip(x,y))
 
     bpy.ops.mesh.primitive_cylinder_add(
         radius = r, 
         depth = dist,
-        location = (mid)   
+        location = mid,
+        # vertices=8,
     ) 
 
     phi = math.atan2(d[1], d[0]) 
@@ -79,6 +91,9 @@ def parse_gmap(darts,dim):
     # Create 'Dart' collection
     change_focus_collection("Darts")
     
+    dart_parsing = time.perf_counter()
+    sphere_creation = 0
+    
     for dart in darts:
         
         # extract information
@@ -93,12 +108,16 @@ def parse_gmap(darts,dim):
         # store position
         dart_id = dart['id']
         id_to_pos[dart_id]=dart_position
-                    
+        
+        sphere_time=time.perf_counter()  
         # make sphere
         bpy.ops.mesh.primitive_uv_sphere_add(
             location=dart_position,
-            radius=DART_radius
+            radius=DART_radius,
+            # segments = 8,
+            # ring_count = 6,
             )
+        sphere_creation += time.perf_counter() - sphere_time
         
         # fix dart Name
         bpy.data.objects["Sphere"].name = "Dart"+str(dart["id"])
@@ -109,7 +128,12 @@ def parse_gmap(darts,dim):
             neighbor = alphas[d]
             if neighbor < dart_id:
                 arcs[d].append((dart_id,neighbor))
-        
+                
+    print("dart parsing time:",time.perf_counter()-dart_parsing)
+    print("sphere creation time:",sphere_creation)
+    
+    arc_parsing = time.perf_counter()
+    
     for d in range(dim+1):
         
         # Create 'Alpha' collection
@@ -127,18 +151,28 @@ def parse_gmap(darts,dim):
             
             # fix name
             bpy.data.objects["Cylinder"].name = str(arc[0]) +  "to" +  str(arc[1])
+    
+    print("arc parsing time:",time.perf_counter()-arc_parsing)
         
 def main():
     try:
+        start_loading = time.perf_counter()
         body = requests.get('http://localhost:8080/modeler')
         darts = (body.json())['result']['gmaps'][0]['dartArray']
         dim = (body.json())['result']['dimension']
-        parse_gmap(darts,dim)         
+        print("loading time:",time.perf_counter()-start_loading)         
+        parse_gmap(darts,dim)
             
     except:
         ShowMessageBox("Erreur during load of Jerboa inside Blender","Erreur", 'ERROR')
-    
+        
+def test():
+    bpy.ops.mesh.primitive_uv_sphere_add(
+        segments = 8,
+        ring_count = 6,
+        location=(-1,-1,-1),
+        radius=0.1,
+        )
     
 if __name__== "__main__":
     main()
-    
